a48c16c778bba1782aa0d79882861a6b64a7c8f9
cafebabe007 <shilovanna@gmail.com>
2017-10-14 22:36:03 -0700

    Separating main form into three partials card, form, preview Updating css styling to comply with media screens Adding text input counter into site.js Adding a model for our greeting card Adding reCAPTCHA form

---
+++ halloween/Models/Greetings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace halloween.Models
+{
+    public class Greetings
+    {
+        [DisplayName("Send To Name")]
+        [Required(ErrorMessage="This field is required.")]
+        public string SendTo { get; set; }
+
+        [DisplayName("Send To Email")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string SendersEmail { get; set; }
+
+        [DisplayName("From")]
+        public string FromName { get; set; }
+
+        [DisplayName("Email")]
+        public string FromEmail { get; set; }
+
+        [DisplayName("Subject")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Subject { get; set; }
+
+        [DisplayName("Personalized Message")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        public string CreateDate { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        public string CreateIP { get; set; }
+       
+        public string SendDate { get; set; }
+        public string SendIP { get; set; }
+
+    }
+}
--- halloween/Pages/Index.cshtml.cs
+++ halloween/Pages/Index.cshtml.cs
@@ -1,20 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using halloween.Models;
 
 namespace halloween.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Message = ""; 
-        
-        //Default: get the request
+
+        //BUILD A BRIDGE 
+        [BindProperty]
+        public Greetings bridgeGreetings { get; set; }
+
+
+        public bool isPreviewPage { get; set; }
+
+
+        //DEFAULT LOAD 
         public void OnGet()
         {
-            Message = "Hi, Anna!";
+            isPreviewPage = false; 
         }
+
+        //PREVIEW MODE 
+        [HttpPost]
+        public void OnPost()
+        {
+            isPreviewPage = true;
+        }
+
+      
+    
+        //public bool isPreviewPage { get; set; }
+        //public string validationMessage { get; set; }
+
+        ////BINDING PROPERTIES
+        //[BindProperty]
+        //public ContactForm ContactForm { get; set; }
+
+        ////Default: FORM INTIALLY LOADS
+        //public void OnGet()
+        //{
+        //    isPreviewPage = false;
+        //    validationMessage = ""; 
+        //}
+
+        ////FORM IS SUBMITTED 
+        //public void OnPost()
+        //{
+        //    isPreviewPage = true;
+
+        //    if (string.IsNullOrEmpty(ContactForm.ToName))
+        //    {
+        //        validationMessage = "Please enter who you are sending to";
+        //        isPreviewPage = false; 
+        //    }
+        //}
+
+
     }
 }
