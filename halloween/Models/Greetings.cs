45aeebcd9a7209ae7521dba979f279ec33ad0615
cafebabe007 <shilovanna@gmail.com>
2017-10-28 21:31:58 -0700

    Adding database Creating a preview form

--- halloween/Models/Greetings.cs
+++ halloween/Models/Greetings.cs
@@ -9,8 +9,11 @@ namespace halloween.Models
 {
     public class Greetings
     {
-        //[DisplayName("Send To")]
-        //[Display(Prompt = "eg Jane")] //this is the placeholder 
+
+        //HEY, Add a unique identifier 
+        [Key]
+        public int ID { get; set; }
+       
         [Display(Name = "Send To", Prompt = "eg Jane")]
         [Required(ErrorMessage="This field is required.")]
         [StringLength(100, MinimumLength =3, ErrorMessage ="Name should be greater than 3, less than a 100.")]
@@ -36,8 +39,14 @@ namespace halloween.Models
         [Required(ErrorMessage = "This field is required.")]
         public string Message { get; set; }
 
+        [Display(Name = "Terms and Conditions")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You gotta tick the box!")]
+        public bool TermsAndConditions { get; set; }
+
         [Required(ErrorMessage = "This field is required.")]
         public string CreateDate { get; set; }
+
+        public string reCaptcha { get; set; }
 
         [Required(ErrorMessage = "This field is required.")]
         public string CreateIP { get; set; }
--- halloween/Pages/Index.cshtml.cs
+++ halloween/Pages/Index.cshtml.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using halloween.Models;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace halloween.Pages
 {
@@ -9,53 +14,109 @@ namespace halloween.Pages
 
         //BUILD A BRIDGE 
         [BindProperty]
-        public Greetings bridgeGreetings { get; set; }
+        public Greetings Greetings { get; set; }
 
+        //Connection to db
+        private Database _dbContext { get; set; }
 
         public bool isPreviewPage { get; set; }
 
+        //hey, Create the database connection through the constructor
+        public IndexModel(Database dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         //DEFAULT LOAD 
         public void OnGet()
         {
-            isPreviewPage = false; 
+            isPreviewPage = false;
         }
 
         //PREVIEW MODE 
         [HttpPost]
-        public void OnPost()
+        public async Task<IActionResult> OnPost()
         {
-            isPreviewPage = true;
+            //call the database 
+
+            if (await isValid())
+            {
+                if (ModelState.IsValid)
+                {
+                    //try
+                    //{
+
+                        // ADD TO DATABASE through instance of our form
+                        _dbContext.Greetings.Add(Greetings);
+                        _dbContext.SaveChanges(); 
+
+                        //REDIRECT to the page with a new operator (name/value pair)
+                        return RedirectToPage("Preview", new { id = Greetings.ID} );
+                    //}
+
+                    //catch { }
+
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Greetings.reCaptcha", "Please verify you're not a robot!");
+            }
+
+            return Page();
+
+
+            // if (ModelState.IsValid)
+            //{
+            //some action 
+            //add to db 
+            // speaks to db -> databaseContext 
+            //_dbContext.Greetings.Add(Greetings);
+            //_dbContext.SaveChanges();
+
+            //return RedirectToPage("Preview", new { id = Greetings.ID });
+            //}
+
+
+            //return RedirectToPage("Index");
+
         }
 
-      
-    
-        //public bool isPreviewPage { get; set; }
-        //public string validationMessage { get; set; }
+        //reCAPTHCA SERVER SIDE VALIDATION  
+        private async Task<bool> isValid()
+        {
+            var response = this.HttpContext.Request.Form["g-recaptcha-response"];
+            if (string.IsNullOrEmpty(response))
+                return false;
 
-        ////BINDING PROPERTIES
-        //[BindProperty]
-        //public ContactForm ContactForm { get; set; }
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var values = new Dictionary<string, string>();
+                    values.Add("secret", "6LfVpjEUAAAAAK0FdygAgh0P1gZ8QU24ildwT86r");
+                    values.Add("response", response);
+                    //values.Add("remoteip", this.HttpContext.Connection.RemoteIpAddress.ToString()); 
 
-        ////Default: FORM INTIALLY LOADS
-        //public void OnGet()
-        //{
-        //    isPreviewPage = false;
-        //    validationMessage = ""; 
-        //}
+                    var query = new FormUrlEncodedContent(values);
 
-        ////FORM IS SUBMITTED 
-        //public void OnPost()
-        //{
-        //    isPreviewPage = true;
+                    var post = client.PostAsync("https://www.google.com/recaptcha/api/siteverify", query);
 
-        //    if (string.IsNullOrEmpty(ContactForm.ToName))
-        //    {
-        //        validationMessage = "Please enter who you are sending to";
-        //        isPreviewPage = false; 
-        //    }
-        //}
+                    var json = await post.Result.Content.ReadAsStringAsync();
 
+                    if (json == null)
+                        return false;
+
+                    var results = JsonConvert.DeserializeObject<dynamic>(json);
+
+                    return results.success;
+                }
+
+            }
+            catch { }
+
+            return false;
+        }
 
     }
 }
