9de4e0e23723c0d1f2612193e428a65ef872a3c0
cafebabe007 <shilovanna@gmail.com>
2017-12-13 22:39:14 -0800

    creating an email template

---
+++ halloween/Pages/Email.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using halloween.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace halloween.Pages
+{
+    public class EmailModel : PageModel
+    {
+        [BindProperty]
+        public Greetings Greetings { get; set; }
+        private Database _dbContext { get; set; }
+        private IConfiguration _configuration { get; set; }
+
+        //hey, Create the database connection through the constructor
+        public EmailModel(Database dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+        }
+
+        public void OnGet(int ID = 0)
+        {
+            if (ID > 0)
+            {
+                Greetings = _dbContext.Greetings.Find(ID);
+            }
+        }
+
+    }  
+}
